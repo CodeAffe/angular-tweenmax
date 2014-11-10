fc650886de1667fc736871d28ffad515d3884003
Pepijn Senders <p.senders92@gmail.com>
2014-11-10 11:17:32 +0100

    more semantic naming

--- src/services/Scroller.js
+++ src/services/Scroller.js
@@ -1,6 +1,6 @@
 tweenmaxApp.service('Scroller', function(TweenMax) {
 
-  this.scrollTo = function(duration, options) {
+  this.to = function(duration, options) {
     if (navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i)) {
       window.scrollTo(x, y);
     } else if (navigator.userAgent.match(/Firefox/i) || navigator.userAgent.match(/MSIE/i)) {
